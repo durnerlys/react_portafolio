74e216f3ff2b84cf8b655d19aa602ea03210ff38
Durnerlys <durnerlys@gmail.com>
2020-05-05 17:08:41 -0400

    Updates

---
+++ precache-manifest.de1388cc97901a877526de25390e527c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a612176e7df4701dc2ab8bac748b941",
+    "url": "/react_portafolio/index.html"
+  },
+  {
+    "revision": "694c55701e0df432a6b3",
+    "url": "/react_portafolio/static/css/2.b548c4be.chunk.css"
+  },
+  {
+    "revision": "8440bfe9f93f792d83b3",
+    "url": "/react_portafolio/static/css/main.161c61cc.chunk.css"
+  },
+  {
+    "revision": "694c55701e0df432a6b3",
+    "url": "/react_portafolio/static/js/2.8cd4da91.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react_portafolio/static/js/2.8cd4da91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8440bfe9f93f792d83b3",
+    "url": "/react_portafolio/static/js/main.b08b4762.chunk.js"
+  },
+  {
+    "revision": "bd02f390f969321cccc9",
+    "url": "/react_portafolio/static/js/runtime-main.5a9b60b0.js"
+  },
+  {
+    "revision": "ed1d9618814a59f03f63537ffdcaade1",
+    "url": "/react_portafolio/static/media/avatar.ed1d9618.jpg"
+  },
+  {
+    "revision": "97e48839a4c0cd8cb10e4ec1af253b87",
+    "url": "/react_portafolio/static/media/durnerlys.97e48839.jpg"
+  }
+]);
